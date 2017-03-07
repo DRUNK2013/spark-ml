d3f3988e767eade57994e0e5c8c128ecaa911d2f
cane <370534549@qq.com>
2017-03-07 23:50:51 +0800

    decision tree

---
+++ src/main/scala/com/drunk2013/spark/ml/classification/DecisionTreeClassifier.scala
@@ -0,0 +1,8 @@
+package com.drunk2013.spark.ml.classification
+
+/**
+  * Created by len on 17-3-7.
+  */
+class DecisionTreeClassifier {
+
+}
---
+++ src/main/scala/com/drunk2013/spark/ml/classification/ProbabilisticClassifier.scala
@@ -0,0 +1,9 @@
+package com.drunk2013.spark.ml.classification
+
+import org.apache.spark.ml.param.shared._
+/**
+  * Created by len on 17-3-7.
+  */
+trait ProbabilisticClassifier /*extends ClassifierParams*/{
+
+}
