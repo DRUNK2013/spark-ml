00115679e33779c6d57b61b7bce6a6bd9af93c6a
cane <370534549@qq.com>
2017-03-08 21:46:40 +0800

    delete file

--- src/main/scala/com/drunk2013/spark/ml/classification/ProbabilisticClassifier.scala
+++
@@ -1,9 +0,0 @@
-package com.drunk2013.spark.ml.classification
-
-import org.apache.spark.ml.param.shared._
-/**
-  * Created by len on 17-3-7.
-  */
-trait ProbabilisticClassifier /*extends ClassifierParams*/{
-
-}
